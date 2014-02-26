96bd3214d21c11d63645721e8285d3f28209f2b1
Giovanni <giovanni@giovanni.(none)>
2014-02-26 15:00:21 +0000

    complete extensions

--- grounded.pl
+++ grounded.pl
@@ -7,11 +7,23 @@ selfdefends(A) :- argument(A),
                forall( (argument(X), attacks(X,A)),
                        (attacks(Y, X), Y\==X, Y\==A, grounded(Y)) ).
 
+%given a list of arguments As, finds if it defends itelf
+defendedby(A,_) :- unattacked(A). 
+defendedby(A,D) :- attacks(X,A),
+		   member(Y,D),
+		   attacks(Y,X).
+
+%if a set of arguments defends itself
+selfdefendsset(As) :- forall(member(A,As), defendedby(A,As)).
+
 %given a list of arguments As, checks if they are conflict free
 conflictfree(As) :- \+((member(X,As),member(Y,As),attacks(X,Y))).
 
-% Empty set is grounded by definition (needed?)
-grounded(A) :- A==[]. 
+%complete extension
+complete(As) :- selfdefends(As), conflictfree(As).
+
+% Empty set is grounded by definition (needed?), may not be complete?
+grounded([]). 
 
 %examples
 argument(a).
@@ -19,3 +31,4 @@ argument(b).
 argument(c).
 
 attacks(a,b).
+attacks(c,a).
