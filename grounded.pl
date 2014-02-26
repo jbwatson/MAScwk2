954b9fb62b532f44519b9d2bd8f1a9ad62ea1ee6
Giovanni <giovanni@giovanni.(none)>
2014-02-26 14:00:48 +0000

    conflict free

--- grounded.pl
+++ grounded.pl
@@ -1,13 +1,21 @@
-% Grounded if nothing attacks it.
-grounded(A) :- argument(A), \+(attacks(_,A)).
+% unattacked if nothing attacks it.
+unattacked(A) :- argument(A), \+(attacks(_,A)).
 
-% Grounded if for every arg X that attacks A, there exists an arg which
+% Defends itelf if for every arg X that attacks A, there exists an arg which
 % attacks X and is grounded. 
-grounded(A) :- argument(A),
+selfdefends(A) :- argument(A),
                forall( (argument(X), attacks(X,A)),
                        (attacks(Y, X), Y\==X, Y\==A, grounded(Y)) ).
+
+%given a list of arguments As, checks if they are conflict free
+conflictfree(As) :- \+((member(X,As),member(Y,As),attacks(X,Y))).
 
 % Empty set is grounded by definition (needed?)
 grounded(A) :- A==[]. 
 
+%examples
+argument(a).
+argument(b).
+argument(c).
 
+attacks(a,b).
