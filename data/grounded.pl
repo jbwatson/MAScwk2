f4065cb94f56ea3ad42441fa9cc3063d0e3535c1
Giovanni Charles <giovanni.charles@gmail.com>
2014-03-03 17:29:02 +0000

    clean

--- data/grounded.pl
+++ data/grounded.pl
@@ -7,22 +7,12 @@ selfdefends(A) :- argument(A),
                forall( (argument(X), attacks(X,A)),
                        (attacks(Y, X), Y\==X, Y\==A, grounded(Y)) ).
 
-
 %given a list of arguments As, finds if it defends itelf
 defendedby(A,_) :- unattacked(A). 
 defendedby(A,D) :- argument(X), argument(Y),
 		   attacks(X,A),
 		   member(Y,D),
 		   attacks(Y,X).
-
-%if a set of arguments defends itself
-selfdefendsset(As) :- forall(member(A,As), defendedby(A,As)).
-
-%given a list of arguments As, checks if they are conflict free
-conflictfree(As) :- \+((member(X,As),member(Y,As),attacks(X,Y))).
-
-%complete extension
-complete(As) :- selfdefendsset(As), conflictfree(As).
 
 % Empty set is grounded by definition (needed?), may not be complete?
 %grounded([]). 
